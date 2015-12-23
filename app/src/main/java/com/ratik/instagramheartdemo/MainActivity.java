3f5649d39d16b8f6e2ed74735b5b254217c41077
Ratik Sharma <Ratik96@gmail.com>
2015-12-23 16:48:46 +0530

    Add heart animation

--- app/src/main/java/com/ratik/instagramheartdemo/MainActivity.java
+++ app/src/main/java/com/ratik/instagramheartdemo/MainActivity.java
@@ -1,13 +1,123 @@
 package com.ratik.instagramheartdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout guideView;
+    private ImageView heartImageView;
+    private View circleBackground;
+
+    // Animation Helpers
+    private static final DecelerateInterpolator DECCELERATE_INTERPOLATOR
+            = new DecelerateInterpolator();
+    private static final AccelerateInterpolator ACCELERATE_INTERPOLATOR
+            = new AccelerateInterpolator();
+
+    // Gesture Detector
+    GestureDetector detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        guideView = (LinearLayout) findViewById(R.id.guide);
+        heartImageView = (ImageView) findViewById(R.id.heart);
+        circleBackground = findViewById(R.id.circleBg);
+
+        detector = new GestureDetector(this, new GestureListener());
+        mainLayout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                guideView.setVisibility(View.INVISIBLE);
+                return detector.onTouchEvent(event);
+            }
+
+        });
+    }
+
+    // GestureListener Inner Class
+    public class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            heart();
+            return true;
+        }
+    }
+
+    private void heart() {
+        circleBackground.setVisibility(View.VISIBLE);
+        heartImageView.setVisibility(View.VISIBLE);
+
+        circleBackground.setScaleY(0.1f);
+        circleBackground.setScaleX(0.1f);
+        circleBackground.setAlpha(1f);
+        heartImageView.setScaleY(0.1f);
+        heartImageView.setScaleX(0.1f);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+
+        ObjectAnimator bgScaleYAnim = ObjectAnimator.ofFloat(circleBackground, "scaleY", 0.1f, 1f);
+        bgScaleYAnim.setDuration(200);
+        bgScaleYAnim.setInterpolator(DECCELERATE_INTERPOLATOR);
+        ObjectAnimator bgScaleXAnim = ObjectAnimator.ofFloat(circleBackground, "scaleX", 0.1f, 1f);
+        bgScaleXAnim.setDuration(200);
+        bgScaleXAnim.setInterpolator(DECCELERATE_INTERPOLATOR);
+        ObjectAnimator bgAlphaAnim = ObjectAnimator.ofFloat(circleBackground, "alpha", 1f, 0f);
+        bgAlphaAnim.setDuration(200);
+        bgAlphaAnim.setStartDelay(150);
+        bgAlphaAnim.setInterpolator(DECCELERATE_INTERPOLATOR);
+
+        ObjectAnimator imgScaleUpYAnim = ObjectAnimator.ofFloat(heartImageView, "scaleY", 0.1f, 1f);
+        imgScaleUpYAnim.setDuration(300);
+        imgScaleUpYAnim.setInterpolator(DECCELERATE_INTERPOLATOR);
+        ObjectAnimator imgScaleUpXAnim = ObjectAnimator.ofFloat(heartImageView, "scaleX", 0.1f, 1f);
+        imgScaleUpXAnim.setDuration(300);
+        imgScaleUpXAnim.setInterpolator(DECCELERATE_INTERPOLATOR);
+
+        ObjectAnimator imgScaleDownYAnim = ObjectAnimator.ofFloat(heartImageView, "scaleY", 1f, 0f);
+        imgScaleDownYAnim.setDuration(300);
+        imgScaleDownYAnim.setInterpolator(ACCELERATE_INTERPOLATOR);
+        ObjectAnimator imgScaleDownXAnim = ObjectAnimator.ofFloat(heartImageView, "scaleX", 1f, 0f);
+        imgScaleDownXAnim.setDuration(300);
+        imgScaleDownXAnim.setInterpolator(ACCELERATE_INTERPOLATOR);
+
+        animatorSet.playTogether(bgScaleYAnim, bgScaleXAnim, bgAlphaAnim, imgScaleUpYAnim, imgScaleUpXAnim);
+        animatorSet.play(imgScaleDownYAnim).with(imgScaleDownXAnim).after(imgScaleUpYAnim);
+
+        animatorSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                reset();
+            }
+        });
+        animatorSet.start();
+    }
+
+    public void reset() {
+        circleBackground.setVisibility(View.GONE);
+        heartImageView.setVisibility(View.GONE);
+        guideView.setVisibility(View.VISIBLE);
     }
 }
